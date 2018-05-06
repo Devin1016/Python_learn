617cf83b624dfad7c23e7a4f09d89fa402540e07
Devin <frisell@vip.qq.com>
2018-05-06 18:15:55 +0800

    fix all

--- tools/all.py
+++ tools/all.py
@@ -9,14 +9,14 @@ from money_challeng import money
 
 
 def main():
-    tools = int(input('工具列表：\n1.汇率计算器\n2.分形树\n3.BMR计算器\n4.存钱计算器\n请选择使用工具：'))
-    if tools == 1:
+    tools = input('工具列表：\n1.汇率计算器\n2.分形树\n3.BMR计算器\n4.存钱计算器\n请选择使用工具：')
+    if tools == '1':
         currency.con_currreny()
-    elif tools == 2:
+    elif tools == '2':
         fractal.fractal()
-    elif tools == 3:
+    elif tools == '3':
         bmr.bmr_final()
-    elif tools == 4:
+    elif tools == '4':
         money.money_challeng()
     else:
         print('不存在你选择的工具。')
